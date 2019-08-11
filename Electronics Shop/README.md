535ce430e6bb2d463ba19fa05166d3f774d1789a
ADELEKE TEMITAYO <lekedevrepo@gmail.com>
2019-08-11 13:44:54 -0600

    add electronic shop exercise

---
+++ Electronics Shop/README.md
@@ -0,0 +1,11 @@
+# Electronics Shop
+
+
+Monica wants to buy a keyboard and a USB drive from her favorite electronics store. The store has several models of each. Monica wants to spend as much as possible for the 2 items, given her budget.
+
+Given the price lists for the store's keyboards and USB drives, and Monica's budget, find and print the amount of money Monica will spend. If she doesn't have enough money to both a keyboard and a USB drive, print -1 instead. She will buy only the two required items.
+
+For example, suppose she has b = 60 to spend. Three types of keyboards cost 
+keyboards = [40, 50, 60]. Two USB drives cost drives = [5, 8, 12]. She could purchase a
+40 keyboard + 12 USB drive = 52, or a 50 keyboard + 8 USB drive = 58. She
+chooses the latter. She can't buy more than 2 items so she can't spend exactly 60.
---
+++ Electronics Shop/electronics-shop.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the getMoneySpent function below.
+ */
+function getMoneySpent(keyboards, drives, b) {
+    let purchase = 0;
+    for(let i = 0; i < keyboards.length; i++){
+        for(let k = 0; k < drives.length; k++){
+            if((keyboards[i] + drives[k]) < b && purchase == 0){
+                purchase = keyboards[i] + drives[k];
+            }else if((keyboards[i] + drives[k]) <= b && (keyboards[i] + drives[k]) > purchase){
+                purchase = keyboards[i] + drives[k];
+            }
+        }
+    }
+    if(purchase == 0){
+        return -1;
+    }else{
+        return purchase;
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const bnm = readLine().split(' ');
+
+    const b = parseInt(bnm[0], 10);
+
+    const n = parseInt(bnm[1], 10);
+
+    const m = parseInt(bnm[2], 10);
+
+    const keyboards = readLine().split(' ').map(keyboardsTemp => parseInt(keyboardsTemp, 10));
+
+    const drives = readLine().split(' ').map(drivesTemp => parseInt(drivesTemp, 10));
+
+    /*
+     * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+     */
+
+    let moneySpent = getMoneySpent(keyboards, drives, b);
+
+    ws.write(moneySpent + "\n");
+
+    ws.end();
+}
