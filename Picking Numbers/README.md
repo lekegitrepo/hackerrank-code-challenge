bc7c8904850f511c0fb22670b766d54c8e0ed371
ADELEKE TEMITAYO <lekedevrepo@gmail.com>
2019-08-11 13:55:14 -0600

    add picking numbers exercise

---
+++ Picking Numbers/README.md
@@ -0,0 +1,11 @@
+# Picking Numbers
+
+Given an array of integers, find and print the maximum number of integers you can select from the array such that the absolute difference between any two of the chosen integers is less than or equal to 1. For example, if your array is a = [1, 1, 2, 2, 4, 4, 5, 5, 5], you can create two subarrays meeting the criterion: [1, 1, 2, 2] and [4, 4, 5, 5, 5]. The maximum length subarray has 5 elements.
+
+Function Description
+
+Complete the pickingNumbers function in the editor below. It should return an integer that represents the length of the longest array that can be created.
+
+pickingNumbers has the following parameter(s):
+
+* a: an array of integers
---
+++ Picking Numbers/picking-numbers.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    let numArr = 0;
+    for(let i = 0; i <= a.length; i++){
+        let arr = 0;
+        for(let k = 0; k <= a.length; k++){
+            if(a[i] == a[k] || a[i] == a[k]+1){
+                arr++;
+            }
+        }
+        numArr = Math.max(arr, numArr);
+    }
+    return numArr;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
